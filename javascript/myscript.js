f314b4a14d1383b512b6e537ec8ca8eab9dea555
Jack-rm <jackcnsr94@gmail.com>
2021-08-26 17:42:13 +0200

    Added uppercase for list ordering purpose

--- javascript/myscript.js
+++ javascript/myscript.js
@@ -2,7 +2,7 @@ let listaCognomi = ["Bianchi", "Neri", "Rossi", "Verdi", "Gialli"];
 let cognomeUtente = prompt("Inserisci il tuo cognome");
 
 document.getElementById("cognome_user").innerHTML = cognomeUtente;
- --
+cognomeUtente = cognomeUtente.charAt(0).toUpperCase() + cognomeUtente.slice(1);  // trasformo primo carattere del cognome dato in maiuscolo per ordinamento della lista
 listaCognomi.push(cognomeUtente);
 document.getElementById("nuova_lista").innerHTML = listaCognomi;
 document.getElementById("nuova_lista_ordinata").innerHTML = listaCognomi.sort();
