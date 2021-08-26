ee1701c33072238b56cb7b0151d6e74da43cf9b0
Jack-rm <jackcnsr94@gmail.com>
2021-08-26 17:33:14 +0200

    Hotfix for loop and element position in array

--- javascript/myscript.js
+++ javascript/myscript.js
@@ -2,13 +2,12 @@ let listaCognomi = ["Bianchi", "Neri", "Rossi", "Verdi", "Gialli"];
 let cognomeUtente = prompt("Inserisci il tuo cognome");
 
 document.getElementById("cognome_user").innerHTML = cognomeUtente;
+ --
 listaCognomi.push(cognomeUtente);
 document.getElementById("nuova_lista").innerHTML = listaCognomi;
 document.getElementById("nuova_lista_ordinata").innerHTML = listaCognomi.sort();
 
-let posizione = 0;
-
-for ( let i = 1; i <= listaCognomi.length; i++){
-    let posizione = listaCognomi.indexOf(cognomeUtente);
+// for ( let i = 0; i <= listaCognomi.length; i++){
+    let posizione = listaCognomi.indexOf(cognomeUtente) + 1;
     document.getElementById("posizione_lista").innerHTML = posizione;
-}
+// }
